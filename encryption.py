088bcd6227d85dc058d7faac3d31c3c984fb27e9
zabile <69739501+Hionakick@users.noreply.github.com>
2021-09-29 22:27:33 +0200

    Add files via upload

--- encryption.py
+++ encryption.py
@@ -1,13 +1,21 @@
 from cryptography.fernet import Fernet
-from pyfiglet import Figlet
+from rich.text import Text
+from rich.console import Console
+from rich import print
+from rich.panel import Panel
 
-f = Figlet(font="puffy")
-print (f.renderText("CR1PT"))
+console = Console()
+text = Text("CRYPT", justify = 'center')
+text.stylize("bold red")
+console.print(text)
 
 while True:
-    choix = input("1.Crypter un fichier    |       2.Decrypter un fichier").lower()
+    choixPrint = Text("1.Crypter un fichier    |    2.Decrypter un fichier    |   3.Quitter le programme", justify = 'center')
+    choixPrint.stylize("bold white")
+    choix = input(console.print(choixPrint)).lower()
 
     if choix == "1":
+        nomFichier = input("Quelle est le nom de votre fichier")
         def makeKey():
             key = Fernet.generate_key()
             with open("key.key", "wb") as keyFile:
@@ -21,21 +29,28 @@ while True:
 
         f = Fernet(key)
 
-        with open('passwd', 'rb') as file:
+        with open(nomFichier, 'rb') as file:
             original = file.read()
 
         encrypted = f.encrypt(original)
 
-        with open('passwd', 'wb') as encrypted_files:
+        with open(nomFichier, 'wb') as encrypted_files:
             encrypted_files.write(encrypted)
 
     if choix == "2":
+        nomFichier = input("Quelle est le nom de votre fichier")
+        def readKey():
+            return open("key.key", "rb").read()
+        key = readKey()
         f = Fernet(key)
 
-        with open('passwd', 'rb') as encrypted_files:
+        with open(nomFichier, 'rb') as encrypted_files:
             encrypted = encrypted_files.read()
 
         decrypted = f.decrypt(encrypted)
 
-        with open('passwd', 'wb') as dec_file:
+        with open(nomFichier, 'wb') as dec_file:
             dec_file.write(decrypted)
+
+    if choix == "3":
+        quit()
